9563e4627e820405a4c4af41e47d8cd196dd7e66
urlexmario <mario.schulz@urlaubsexperte.de>
2026-08-25 13:52:15 +0200

    feat: add nvim-cmp plugin

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+    },
+    config = function()
+      -- ...
+    end,
+  },
+}
