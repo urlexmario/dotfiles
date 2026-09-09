06761366419c3200002d32642b41b6cd247cc8df
urlexmario <mario.schulz@urlaubsexperte.de>
2026-09-09 13:39:48 +0200

    chore: reset config files

--- lua/plugins/telescope.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "nvim-telescope/telescope.nvim", version = "*",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim" }
-    }
-  },
-}
